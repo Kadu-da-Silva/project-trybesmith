2cb917af6781363110d3d69b5a1549de13279e38
Carlos Eduardo da Silva <carlao0010@hotmail.com>
2023-09-15 12:36:39 -0300

    Rota OK

---
+++ src/controllers/products.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import productsService from '../services/products.service';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
+
+async function create(req: Request, res: Response) {
+  try {
+    const { name, price, orderId } = req.body;
+    const serviceResponse = await productsService.create({ name, price, orderId });
+    if (serviceResponse.status !== 'SUCCESSFUL') {
+      return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+    }
+    res.status(201).json(serviceResponse.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+export default {
+  create,
+};
